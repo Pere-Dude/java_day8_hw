4c8e8497f9ab4414bd2117e7d7b73e693846043b
Максим Евсеев <vailogiant@gmail.com>
2023-04-12 23:59:54 +0500

    Рассчёт доставки карты

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        int deliveryDistance = 40;
+        cardDeliveryTime(deliveryDistance);
+    }
 
+    public static void cardDeliveryTime(int distance) {
+        if ((distance > 0) && (distance <= 60)) {
+            int deliveryTime = 0;
+            for (int i = 0; i < distance; i += 20) {
+                deliveryTime++;
+            }
+            System.out.println("Потребуется дней: " + deliveryTime);
+        } else {
+            System.out.println("Доставки нет");
+        }
     }
 }
